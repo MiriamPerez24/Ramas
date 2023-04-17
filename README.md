004c954b03c3c16637f06b4e86344f6f94789eeb
MiriamPerez24 <102840011+MiriamPerez24@users.noreply.github.com>
2023-04-16 21:39:18 -0700

    Ramitas

---
+++ README.md
@@ -0,0 +1,12 @@
+# PROYECTO RAMITAS 🌳
+
+Este proyecto es para repasar el concepto de ramas.
+
+---
+### CONCEPTO:
+
+1. git clone 
+2. markdown README.md
+3. Dejamos de ser racistas con el comando "git branch -M main"
+4.  Pasamos al area de staging 
+con el comando "git add --all"
---
+++ main.py
@@ -0,0 +1,27 @@
+"Esto permite que solo se exporte el words que es una lista que es un conjunto de elementos "
+from  example import words 
+
+from suma import suma 
+from subtract import resta
+from multiplicar import suma 
+from division import suma
+
+# Proyecto para practicar ramas 
+
+"""
+Comentario 
+Multilinea 
+en python
+"""
+
+print("Basic operations")
+
+print (words)
+
+# Addition operation 
+
+# Rest operation 
+
+# Multiplicaction operation 
+
+# Division operation 
